344077b452d1751627be065b19d2858e62762ef8
Austin Crane <amcrane1@wichita.edu>
2016-08-17 11:17:44 -0500

    added more detailed comments on the code and did some refactoring

--- lib/liquidplanner/liquidplanner.rb
+++ lib/liquidplanner/liquidplanner.rb
@@ -1,83 +1,104 @@
-##
-class Lptimekeeping::LiquidPlanner
-  include HTTParty
+require 'uri'
+require 'httparty'
+module Lptimekeeping
+  ## An interface for the liquid planner api
+  # at its current state it is non distructive so it is pretty safe to use
+  class LiquidPlanner
+    # The library used to make HTTP requests to liquid planner
+    include HTTParty
+    # The origin of the liquid planner api
+    base_uri 'https://app.liquidplanner.com/api'
+    # make all data formats comming in json
+    format :json
 
-  base_uri 'https://app.liquidplanner.com/api'
-  format :json
+    # Requires some basic authentication to make requests to liquidplanner
+    # so we init with credintials
+    def initialize(email, password)
+      # opts is now a class property, it is a hash
+      @opts = { basic_auth: { username: email, password: password },
+                headers: { 'content-type' => 'application/json' } }
+    end
 
-  def initialize(email, password)
-    @opts = { :basic_auth => { :username => email, :password => password },
-              :headers    => { 'content-type' => 'application/json' },
-            }
-  end
+    # Governed get request, all requests add basic auth from the opt
+    def get(url, options = {})
+      self.class.get(url, options.merge(@opts)) # merge opts to request headers
+    end
 
-  def get(url, options={})
-    self.class.get(url, options.merge(@opts))
-  end
+    # Governed post request, all requests add basic auth from the opt
+    def post(url, options = {})
+      # grabs the body from the options param to use for data in post request
+      options[:body] = options[:body].to_json if options[:body]
+      self.class.post(url, options.merge(@opts)) # merge opts to request headers
+    end
 
-  def post(url, options={})
-    options[:body] = options[:body].to_json if options[:body]
-    self.class.post(url, options.merge(@opts))
-  end
+    # Governed put request, all requests add basic auth from the opt
+    def put(url, options = {})
+      # grabs the body from the options param to use for data in post request
+      options[:body] = options[:body].to_json if options[:body]
+      self.class.put(url, options.merge(@opts)) # merge opts to request headers
+    end
 
-  def put(url, options={})
-    options[:body] = options[:body].to_json if options[:body]
-    self.class.put(url, options.merge(@opts))
-  end
+    # Get user account info
+    # An example output can be found on page 7 of LP api documentation
+    def account
+      get('/account')
+    end
 
-  def account
-    get('/account')
-  end
+    # Get all of the users workspaces
+    # gets a list of workspaces that a user has access too, workspaces are
+    # important to give scope to other requests
+    def workspaces
+      get('/workspaces')
+    end
 
-  def workspaces
-    get('/workspaces')
-  end
+    # Get all projects for a workspace, given that the user has access to the
+    # workspace
+    def projects(workspace_id)
+      get("/workspaces/#{workspace_id}/projects")
+    end
 
-  def projects(workspace_id)
-    get("/workspaces/#{workspace_id}/projects")
-  end
+    # Get all tree items under a project, this includes folders and tasks
+    def tasks(workspace_id, project_id)
+      get("/workspaces/#{workspace_id}/treeitems/#{project_id}?depth=-1&leaves=true")
+    end
 
-  def tasks(workspace_id, project_id)
-    get("/workspaces/#{workspace_id}/treeitems/#{project_id}?depth=-1&leaves=true")
-  end
+    # Starts a timer on a task by task_id
+    def start_timer(workspace_id, task_id)
+      post("/workspaces/#{workspace_id}/tasks/#{task_id}/timer/start")
+    end
 
-  def start_timer(workspace_id, task_id)
-    post("/workspaces/#{workspace_id}/tasks/#{task_id}/timer/start")
-  end
+    # Stops a timer that is running on a task by task_id
+    def stop_timer(workspace_id, task_id)
+      post("/workspaces/#{workspace_id}/tasks/#{task_id}/timer/stop")
+    end
 
-  def stop_timer(workspace_id, task_id)
-    post("/workspaces/#{workspace_id}/tasks/#{task_id}/timer/stop")
-  end
+    # Clear a timer that is running on a task by task_id
+    def clear_timer(workspace_id, task_id)
+      post("/workspaces/#{workspace_id}/tasks/#{task_id}/timer/clear")
+    end
 
-  def clear_timer(workspace_id, task_id)
-    post("/workspaces/#{workspace_id}/tasks/#{task_id}/timer/clear")
-  end
+    # Commit a timer, this adds the total time on the timer to the timesheet,
+    # commit by task_id
+    def commit_timer(workspace_id, task_id)
+      post("/workspaces/#{workspace_id}/tasks/#{task_id}/timer/commit", body: { activity_id: 226386 })
+    end
 
-  def commit_timer(workspace_id, task_id)
-    post("/workspaces/#{workspace_id}/tasks/#{task_id}/timer/commit", :body => {:activity_id => 226386})
-  end
+    # Gets all of the currently running timers for user
+    def timers(workspace_id)
+      get("/workspaces/#{workspace_id}/my_timers")
+    end
 
-  def timers(workspace_id)
-    get("/workspaces/#{workspace_id}/my_timers")
-  end
-
-  def current_task(workspace_id)
-    timers = get("/workspaces/#{workspace_id}/my_timers")
-    if timers.count > 0
-      return timers.first['item_id']
-    else
-      return nil
+    # Current Task being worked on, if there is not task nil is returned
+    # this is not a built in feature to liquid planner but helps
+    def current_task(workspace_id)
+      timers = get("/workspaces/#{workspace_id}/my_timers")
+      # TODO: figure out a good way to do one line if statements in ruby,
+      # lexical?
+      if timers.count > 0
+        return timers.first['item_id']
+      else
+        return nil
+      end
     end
   end
-
-  # def create_task(data)
-  #   options = { :body => { :task => data } }
-  #   post("/workspaces/#{workspace_id}/tasks", :body => { :task => data })
-  # end
-  #
-  # def update_task(data)
-  #   options = { :body => { :task => data } }
-  #   put("/workspaces/#{workspace_id}/tasks/#{data['id']}", :body => { :task => data })
-  # end
-
 end
--- lib/lptimekeeping.rb
+++ lib/lptimekeeping.rb
@@ -1,74 +1,113 @@
+# include the versioning file for lptimekeeping
 require 'lptimekeeping/version'
-require 'uri'
-require 'httparty'
+# for some reason highline is require for the ask() method
 require 'highline/import'
+# include our interface for liquidplanner api
 require 'liquidplanner/liquidplanner'
 
-##
 module Lptimekeeping
-  ## Terminal interface for liquidplanner
+  ## Terminal interface for liquidplanner timekeeping
+  # The current method for keeping track of time is through timers that are
+  # tied to tasks, so to clock in you set a timer on a task and to clock out
+  # you stop the timer and commit the total time for the timer to the task.
   class Terminal
+    # accessible class attributes
     attr_accessor :workspace, :project, :task, :lp
 
+    # New Terminal, this asks for the user credintials if the user does not
+    # have enviroment variables set for their account info, this feature might
+    # get taken out due to some security risks
     def initialize
+      # check if the users system has enviroment variables set for their
+      # liquid planner credintials
       if ENV['LP_USERNAME'] && ENV['LP_PASSWORD']
+        # use the enviroment vars for credintials
         email, password = ENV['LP_USERNAME'], ENV['LP_PASSWORD']
       else
+        # ask user for credintials through the command line
         email, password = credintials
       end
+      # initialize the instance variable for our liquid planner interface
       @lp = LiquidPlanner.new(email, password)
     end
 
+    # A menu to use for the options available to the user, this is just a string
+    # nothing fancy here
     def option_prompt
       "\nOptions:\n\t1. Timekeeping\n\t2. List Tasks\n\t3. Exit\n"
     end
 
+    # Returns the first workspace available in the users workspaces
+    # TODO: this could be simplified as a design pattern
     def select_workspace
+      # Gets all the users workspaces
       workspaces = @lp.workspaces
+      # Gets the first one out of the array
       ws = workspaces.first
-      return ws['id']
+      # returns the value for the id of the project
+      ws['id']
     end
 
+    # Asks the user for their credintials for liquid planner
+    # and returns email, password
     def credintials
-      email    = ask("LiquidPlanner email: ")
-      password = ask("LiquidPlanner password for #{email}: ") {|q| q.echo = false}
+      # prompts user for email
+      email    = ask('LiquidPlanner email: ')
+      # prompts user for password
+      password = ask('LiquidPlanner password for #{email}: ') { |q| q.echo = false }
+      # return the inputed values
+      # TODO: maybe do some validation here?
       return email, password
     end
 
     def scope_project
+      # Get all of the projects for the selected workspace
       projects = @lp.projects(@workspace)
+      # Tell the user how many projects were found in the workspace
       puts "\nThese are the #{projects.length} projects in your workspace"
+      # for each project in projects print out a description of the options
       projects.each_with_index do |p, i|
-      puts " #{i+1}. #{p['name']}"
+        puts " #{i + 1}. #{p['name']} (id: #{p['id']})"
       end
-      project_selected = projects[ask("Set the project you want to look at:").to_i-1]
+      # get the index of the project, need to -1 here sense we start at 1 and
+      # not 0
+      index = ask('Set the project you want to look at:').to_i - 1
+      # get the selected project out of the array
+      project_selected = projects[index]
+      # set it as the current scope for project
       @project = project_selected['id']
     end
 
+    # Print out tasks to terminal
     def print_tasks
+      # Get all of the tasks
       tasks = @lp.tasks(@workspace, @project)
       puts "\nTasks:"
       tasks['children'].each_with_index do |t, i|
-        puts "\t#{i+1}. #{t['name']} (id: #{t['id']})"
+        puts "\t#{i + 1}. #{t['name']} (id: #{t['id']})"
       end
     end
 
+    # Select the task to use for scope
+    # TODO: this and print_tasks could be simplified together
     def scope_task
+      # Get all of the tasks
       tasks = @lp.tasks(@workspace, @project)
       puts "\nTasks:"
       if tasks['children']
         tasks['children'].each_with_index do |t, i|
-          puts "\t#{i+1}. #{t['name']} (id: #{t['id']})"
+          puts "\t#{i + 1}. #{t['name']}"
         end
-        task_selected = tasks['children'][ask("Set the task you want to choose:").to_i-1]
+        index = ask('Set the task you want to choose:').to_i - 1
+        task_selected = tasks['children'][index]
         @task = task_selected['id']
       else
-        puts "This Project has no tasks..."
+        puts 'This Project has no tasks...'
       end
     end
 
     def divider
-      puts "--------------------------"
+      puts '--------------------------'
     end
 
     def list_timers
@@ -76,7 +115,7 @@ module Lptimekeeping
       if timers.count > 0
         puts timers
       else
-        puts "You currently have no timers running..."
+        puts 'You currently have no timers running...'
       end
     end
 
@@ -85,11 +124,12 @@ module Lptimekeeping
     end
 
     def clock_out
-      @lp.stop_timer(@workspace,@task)
-      @lp.commit_timer(@workspace,@task)
+      @lp.stop_timer(@workspace, @task)
+      @lp.commit_timer(@workspace, @task)
       @lp.clear_timer(@workspace, @task)
     end
 
+    # TODO: change this to clocked_in?
     def is_clocked_in
       if @lp.timers(@workspace).count == 0
         return false
@@ -100,11 +140,11 @@ module Lptimekeeping
 
     def timekeeping
       if is_clocked_in == true
-        option = ask("You are clocked in, would you like to clock out?(y/n)")
-        if option == "y"
+        option = ask('You are clocked in, would you like to clock out?(y/n)')
+        if option == 'y'
           clock_out
         else
-          puts "You were not clocked out"
+          puts 'You were not clocked out'
         end
       else
         divider
@@ -123,18 +163,18 @@ module Lptimekeeping
       @task = @lp.current_task(@workspace)
       while should_prompt
         option = ask(option_prompt)
-        if option == "1"
+        if option == '1'
           timekeeping
         end
-        if option == "2"
+        if option == '2'
           scope_project
           divider
           print_tasks
         end
-        # if option == "3"
+        # if option == '3'
         #   list_timers
         # end
-        if option == "3"
+        if option == '3'
           should_prompt = false
         end
         divider
